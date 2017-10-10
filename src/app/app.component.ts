3b51a8af8abf57404324f83d7e05092cf3cb28b9
Liseth Cardozo <cardozoliseth@gmail.com>
2017-10-10 15:16:31 -0700

    Initial Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  emails = [
+    {email: "angel@gmail.com", importance: true, subject: "Programing", content: "coding is fun!"},
+    {email: "CAR@gmail.com", importance: true, subject: "Algos", content: "Come to algos!"
+    },
+    {email: "Ariel@gmail.com", importance: false, subject:  "New Window", content: "Window 10 coming in summer"},
+    {email: "alex@gmail.com", importance: false, subject: "party", content: "just kidding"}
+  ]
 }
